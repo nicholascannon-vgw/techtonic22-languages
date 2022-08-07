41db9f41a2208964efa7417251c36cdf880537e3
Nic <nicholas.cannon@vgw.co>
2022-08-07 17:36:43 +0800

    Rust server

---
+++ rust/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MessageResponse {
+    message: String,
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> std::io::Result<impl Responder> {
+    Ok(web::Json(MessageResponse {
+        message: "healthy".to_string()
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(healthcheck)
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
