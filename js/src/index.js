6385f41eeb7b961dd9530556c10eb017403d9652
Nic <nicholas.cannon@vgw.co>
2022-08-10 17:54:02 +0800

    Add scrappy website

--- js/src/index.js
+++ js/src/index.js
@@ -4,6 +4,13 @@ const { isEmptyString, notString } = require('./validation');
 const app = express();
 app.use(express.json());
 
+app.use((_req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+});
+
 app.get('/healthcheck', (_req, res) => {
     return res.json({ message: 'healthy' });
 });
@@ -28,7 +35,7 @@ app.post('/count', (req, res) => {
         wordCount.set(sanitizedWord, count + 1);
     }
 
-    return res.json({ wordCount: Object.fromEntries(wordCount) });
+    return res.json({ ...Object.fromEntries(wordCount) });
 });
 
 const server = app.listen(8000, () => console.log('Service started on port 8000...'));
