915e699f47ce72dfe96d4c76f932f33846c27bed
Nic <nicholas.cannon@vgw.co>
2022-08-12 11:38:36 +0800

    Remove extra langs + simplify docs

--- py/src/__init__.py
+++
@@ -1,1 +0,0 @@
-# this file is required for src to be a python package
--- py/src/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/healthcheck")
-def healthcheck():
-    return { "message": "healthy" }
