5e14690b38a6d34dda25c8983a58643e4d1d4b58
Nic <nicholas.cannon@vgw.co>
2022-08-12 12:08:00 +0800

    Improve go code

--- go/src/main.go
+++ go/src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"techtonic/src/middleware"
 
 	"github.com/gorilla/mux"
 )
@@ -13,13 +14,26 @@ type MessageResponse struct {
 	Message string `json:"message"`
 }
 
+type WordCount map[string]int
+
+func jsonResponse(w http.ResponseWriter, body interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(body)
+}
+
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(MessageResponse{Message: "healthy"})
+		jsonResponse(w, MessageResponse{Message: "healthy"})
 	})
+
+	router.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+		jsonResponse(w, WordCount{"Hello": 1, "world": 1})
+	})
+
+	// Allow the frontend to call this service
+	router.Use(middleware.Cors)
 
 	fmt.Println("Listening on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
---
+++ go/src/middleware/cors.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "net/http"
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers")
+
+		next.ServeHTTP(w, r)
+	})
+}
