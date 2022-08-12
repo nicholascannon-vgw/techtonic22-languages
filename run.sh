5e1f6315439a75b3616ee6d86d3fea48e67cef36
Nic <nicholas.cannon@vgw.co>
2022-08-12 18:02:20 +0800

    run scropt

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+set -e
 
 # silence some docker logs
 export DOCKER_SCAN_SUGGEST=false
@@ -8,5 +8,5 @@ cd $lang
 
 docker build -t techtonic-$lang:lts .
 # remove the container if it failed last time
-docker container rm -f $lang >/dev/null 2>&1
-docker run -p 8000:8000 --name $lang techtonic-$lang:lts
+docker container rm -f techtonic-$lang >/dev/null 2>&1
+docker run -p 8000:8000 --name techtonic-$lang techtonic-$lang:lts
